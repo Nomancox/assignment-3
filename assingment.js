022626fbc20207f54bdb8ed53dfa1ce72b7c0408
Noman Helali <nomancox85@gmail.com>
2021-01-22 21:42:50 -0800

    creating javascript

---
+++ assingment.js
@@ -0,0 +1,46 @@
+kilometerToMeter:
+function kilometerToMeter(kilometer) {
+    var meter = kilometer * 1000;
+    return meter;
+}
+var ratonMeter = kilometerToMeter(50);
+//console.log(ratonMeter);
+var abulMeter = kilometerToMeter(56);
+//console.log(abulMeter);
+var kabulMeter = kilometerToMeter(38);
+//console.log(kabulMeter);
+
+
+budgetCalculator:
+var watch = '10';
+var phone = '15'
+var laptop = '10'
+function add(watch, phone, laptop,) {
+    var result = 10*50 + 15*100 + 10*500;
+    return result;
+}
+var sum = add(500, 1500, 5000);
+//console.log(sum);
+
+HotelCost:
+var tenthDays = '100';
+var ninetenthDays = '80';
+var restDays = '50';
+function add(tenthDays, ninetenthDays, restDays,) {
+    var result = 10*100 + 19*80 + 70*50;
+    return result;
+}
+var sum = add(1000, 1520, 3500);
+//console.log(sum);
+
+
+MegaFriends:
+var friends = [Salam, Karim, Rahim, Hasnat, Kristina, Arman];
+Var maxWord = friends[person];
+for(var i = person; i < friends.length; i++){
+    var element = friends[i];
+    if(element > maxWord){
+        maxWord = element;
+    }
+}
+console.log("largest name is: ", maxWord);
